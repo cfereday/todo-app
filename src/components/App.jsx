637da97736203dc1c98890dc7690ae5a01aeeec3
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-05-17 20:32:18 +0200

    The CheckBox component manages the state
    - And passes it back up to the App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import { CheckBox } from './Checkbox.jsx';
 
 export function App() {
     const [checked, setChecked] = useState(false);
     const handleClick = () => setChecked(!checked);
-    return <input type="checkbox" name="Something to do" onClick={handleClick} checked={checked}/>
+    return <CheckBox onClick={handleClick} isChecked={checked}/>
 }
--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-const CheckBox = (props) => {
+export const CheckBox = (props) => {
     return (
         <React.Fragment>
             <label htmlFor="something-to-do">Something To Do</label>
-            <input id="something-to-do" type="checkbox" checked={props.checked}/>
+            <input id="something-to-do" type="checkbox" checked={props.isChecked} onChange={props.onClick}/>
         </React.Fragment>
     )
-};
-
-module.exports = {
-    CheckBox
 };
 
