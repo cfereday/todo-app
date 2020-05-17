ff17f53d9ed7798dc844c929f06ea913f7f3acde
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-05-17 20:59:46 +0200

    Moves task data into single object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import {ToDo} from './ToDo.jsx';
 
 export function App() {
+    const inputTask = {note: 'Something to do', id: 0};
     const [done, setChecked] = useState(false);
     const handleClick = () => setChecked(!done);
-    return <ToDo onClick={handleClick} isChecked={done} task={"Something To Do"} id={1}/>
+    return <ToDo onClick={handleClick} isChecked={done} task={inputTask}/>
 }
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const ToDo = ({task, onClick, id, done}) => {
+export const ToDo = ({task: {note, id}, onClick, done}) => {
     return (
         <React.Fragment>
-            <label htmlFor={id}>{task}</label>
+            <label htmlFor={id}>{note}</label>
             <input id={id} type="checkbox" checked={done} onChange={onClick}/>
         </React.Fragment>
     )
