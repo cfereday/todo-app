247451480d0a972853ca99f57083e0315b7f5198
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-05-17 21:20:06 +0200

    Mapping over all of the input tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,14 @@ import React, {useState} from 'react';
 import {ToDo} from './ToDo.jsx';
 
 export function App() {
-    const inputTask = {note: 'Something to do', id: 0};
+    const inputTask = [{note: 'Something to do', id: 0}, {note: 'Another to do', id: 1}, {note: 'Additional to do', id: 2}];
     const [done, setChecked] = useState(false);
     const handleClick = () => setChecked(!done);
-    return <ToDo onClick={handleClick} isChecked={done} task={inputTask}/>
+    console.log('inside the app', done);
+
+    return (
+        <div>
+            {inputTask.map(task =>  <ToDo onClick={handleClick} isChecked={done} task={task}/>)}
+        </div>
+    )
 }
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export const ToDo = ({task: {note, id}, onClick, done}) => {
+export const ToDo = ({task: {note, id}, onClick, isChecked}) => {
+    console.log('inside the todo', isChecked);
     return (
         <React.Fragment>
             <label htmlFor={id}>{note}</label>
-            <input id={id} type="checkbox" checked={done} onChange={onClick}/>
+            <input id={id} type="checkbox" checked={isChecked} onChange={onClick}/>
         </React.Fragment>
     )
 };
