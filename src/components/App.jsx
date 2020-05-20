ce79f4501ad9f7d9fe8e1d487b2d8f2deb82f35e
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-05-20 12:18:18 +0200

    State changes on a single ToDo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,11 @@ import React, {useState} from 'react';
 import {ToDo} from './ToDo.jsx';
 
 export function App() {
-    const inputTask = [{note: 'Something to do', id: 0}, {note: 'Another to do', id: 1}, {note: 'Additional to do', id: 2}];
-    const [done, setChecked] = useState(false);
-    const handleClick = () => setChecked(!done);
-    console.log('inside the app', done);
+    const [state, updateState] = useState({task: 'Example note', id: 0, done: false});
 
     return (
         <div>
-            {inputTask.map(task =>  <ToDo onClick={handleClick} isChecked={done} task={task}/>)}
+            {<ToDo onUserChanges={updateState} isChecked={ state.done } task={state.task} id={state.id}/>}
         </div>
     )
 }
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 
-export const ToDo = ({task: {note, id}, onClick, isChecked}) => {
-    console.log('inside the todo', isChecked);
+export const ToDo = ({task, id, isChecked, onUserChanges}) => {
+
+    const handleInputchange = (event) => {
+        const target = event.target;
+
+        if (target.value !== 'Example note') {
+            task = target.value
+        }
+
+        return {task}
+    };
+
+    const handleCheckedBoxChange = (event) => {
+        const target = event.target;
+        isChecked = !!target.checked;
+
+        return {isChecked}
+    };
+
     return (
         <React.Fragment>
-            <label htmlFor={id}>{note}</label>
-            <input id={id} type="checkbox" checked={isChecked} onChange={onClick}/>
+            <form>
+                <label htmlFor={id}>
+                <textarea value={task} onChange={(event) => {
+                    const latestState = handleInputchange(event);
+                    onUserChanges(latestState);
+                }}/>
+                    <input name="toDo" key={id} type="checkbox" checked={isChecked} onChange={(event) => {
+                        const latestState = handleCheckedBoxChange(event);
+                        onUserChanges(latestState);
+                    }}/>
+                </label>
+            </form>
         </React.Fragment>
     )
 };
