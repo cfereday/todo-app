59b08302ee35142bd4476f605635bbd5bbcb4823
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-05-25 12:48:33 +0200

    Initial fix of saving state issue

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -5,12 +5,12 @@ export const ToDo = ({ todo, doneChanged }) => {
         <React.Fragment>
             <input data-testid={`todo-checked-${todo.unique}`} name="toDo" type="checkbox" checked={todo.done}
                    onChange={(event) => {
-                       const gatheredInfo = {unique: todo.unique, done: event.target.checked};
+                       const gatheredInfo = {unique: todo.unique, done: event.target.checked, note: todo.note};
                        doneChanged(gatheredInfo)
                    }}/>
             <input data-testid={`todo-note-${todo.unique}`} name="task" type="text" value={todo.note}
                    onChange={(event) => {
-                       const gatheredInfo = {unique: todo.unique, note: event.target.value};
+                       const gatheredInfo = {unique: todo.unique, done: todo.done, note: event.target.value};
                        doneChanged(gatheredInfo)
                    }}/>
         </React.Fragment>
