3c2470d17f6dd55a1cf6e3dacb208f473f37097b
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-05-31 14:55:01 +0200

    Minor renaming

--- src/components/AddToDoButton.jsx
+++ src/components/AddToDoButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const AddToDoButton = ({ addToDo }) => {
     return (
-        <button data-testid={`todo-button`} className="Button" value="Create new ToDo" onClick={addToDo}>
+        <button data-testid={`new-todo-button`} className="Button" value="Create new ToDo" onClick={addToDo}>
             Add new todo
         </button>
     );
